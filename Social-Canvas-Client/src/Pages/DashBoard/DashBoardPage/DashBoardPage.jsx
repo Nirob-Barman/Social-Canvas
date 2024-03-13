a6b983ef4fb7f799ea2b678a2faad6bfa043b1d6
Nirob-Barman <niroborin111@gmail.com>
2024-03-13 23:08:55 +0600

    Uncommented Posts

--- Social-Canvas-Client/src/Pages/DashBoard/DashBoardPage/DashBoardPage.jsx
+++ Social-Canvas-Client/src/Pages/DashBoard/DashBoardPage/DashBoardPage.jsx
@@ -4,6 +4,7 @@ const DashBoardPage = () => {
     return (
         <div className='min-h-screen'>
             <h1 className='text-3xl text-center'>Welcome to DashBoard</h1>
+            <p className='text-center'>This is your personalized dashboard. Explore and manage your content here.</p>
         </div>
     );
 };
