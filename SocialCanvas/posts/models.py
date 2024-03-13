dd7f8846b5b89964990ca2cfeaa2703e65be84b9
Nirob-Barman <niroborin111@gmail.com>
2024-03-13 09:30:00 +0600

    Review & Post Model updated

--- SocialCanvas/posts/models.py
+++ SocialCanvas/posts/models.py
@@ -10,6 +10,17 @@ class Post(models.Model):
     video_url = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    like_count = models.IntegerField(default=0)
+
+    @property
+    def comment_count_value(self):
+        return self.comments.count()
+    
+    @comment_count_value.setter
+    def comment_count_value(self, value):
+        # You can implement a custom setter logic if needed
+        # For example, you might want to do something with the value
+        pass
 
     def __str__(self):
         return f"{self.user.username}'s Post-{self.id} - {self.content} ({self.created_at})"
@@ -23,6 +34,10 @@ class Like(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='comments', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Comment-{self.id} - {self.content} ({self.created_at})"
