44e4a7ed26c6ccfe1a1852d04bb5cc8fc7bf44fb
Nirob_Barman <niroborin111@gmail.com>
2024-03-31 22:18:30 +0600

    Banner

--- Social-Canvas-Client/src/Pages/Home/Home/Home.jsx
+++ Social-Canvas-Client/src/Pages/Home/Home/Home.jsx
@@ -4,6 +4,7 @@ import TopLikedPosts from "../../DashBoard/TopLikedPosts/TopLikedPosts";
 import TopCommentedPosts from "../../DashBoard/TopCommentedPosts/TopCommentedPosts";
 import Testimonials from "../Testimonials/Testimonials";
 import ContactAndSupport from "../ContactAndSupport/ContactAndSupport";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     const { user } = useAuth();
@@ -14,6 +15,8 @@ const Home = () => {
             <Helmet>
                 <title>Phi Book | Home</title>
             </Helmet>
+
+            <Banner />
 
             {/* Hello {user?.email} */}
 
